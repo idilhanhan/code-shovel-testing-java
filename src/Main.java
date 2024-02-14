46df6547d1be1b9225affc1e5d643538c52ce867
idilhanhan <idilhanhan@gmail.com>
2024-02-14 15:47:25 +0300

    add method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+    public static int getMaximum(int x, int y){
+        if(x >= y){
+            return x;
+        }
+        return y;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Running Main.java");
+        int maxValue = getMaximum(4, 5);
+        System.out.println(maxValue);
     }
 }
