8b8e726f60d2ca99ea6afbad356a218e9cf2f5c9
idilhanhan <idilhanhan@gmail.com>
2024-02-14 17:42:02 +0300

    change method signature and move to another file

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,27 @@ public class Main {
         return sum;
     }
 
+    public static int getMinimum(int x, int y, int z){
+        int min = 0;
+        if (x <= y){
+            min = x;
+        }
+        else{
+            min = y;
+        }
+        if (z <= min){
+            min = z;
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
         System.out.println("Running Main.java");
         int maxValue = getSum(4, 5);
-        //int minValue = Math.getMinimum(4, 5);
+        int minValue = getMinimum(4, 5, 3);
         //boolean result = Math.isEqual(3,3);
         System.out.println(maxValue);
-        //System.out.println(minValue);
+        System.out.println(minValue);
         //System.out.println(result);
     }
 }
--- src/tester/Math.java
+++ src/tester/Math.java
@@ -2,12 +2,6 @@ package tester;
 
 
 class Parent{
-    public static int getMinimum(int x, int y){
-        if (x <= y){
-            return x;
-        }
-        return y;
-    }
 
 }
 
