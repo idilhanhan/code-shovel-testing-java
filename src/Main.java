35e6791d5dc0a9872d5b43ee5db0929065897448
idilhanhan <idilhanhan@gmail.com>
2024-02-14 17:08:20 +0300

    move method to another file

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,10 @@ public class Main {
         return x + y;
     }
 
-    public static int getMinimum(int x, int y){
-        if (x <= y){
-            return x;
-        }
-        return y;
-    }
-
     public static void main(String[] args) {
         System.out.println("Running Main.java");
         int maxValue = getSum(4, 5);
-        int minValue = getMinimum(4, 5);
+        int minValue = Tester.getMinimum(4, 5);
         boolean result = Tester.isEqual(3,3);
         System.out.println(maxValue);
         System.out.println(minValue);
--- src/Tester.java
+++ src/Tester.java
@@ -2,4 +2,11 @@ public class Tester {
     public static boolean isEqual(int x, int y){
         return x == y;
     }
+
+    public static int getMinimum(int x, int y){
+        if (x <= y){
+            return x;
+        }
+        return y;
+    }
 }
