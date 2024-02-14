ff295f297271aee300e909a696759ee93cf31525
idilhanhan <idilhanhan@gmail.com>
2024-02-14 16:05:03 +0300

    add another method

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,18 @@ public class Main {
         return y;
     }
 
+    public static int getMinimum(int x, int y){
+        if (x <= y){
+            return x;
+        }
+        return y;
+    }
+
     public static void main(String[] args) {
         System.out.println("Running Main.java");
         int maxValue = getMaximum(4, 5);
+        int minValue = getMinimum(4, 5);
         System.out.println(maxValue);
+        System.out.println(minValue);
     }
 }
