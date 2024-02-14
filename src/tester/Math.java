c613e62033e3aef663b5e59e729edcd132af8158
idilhanhan <idilhanhan@gmail.com>
2024-02-14 17:47:04 +0300

    move file again

--- src/tester/Math.java
+++ src/tester/Math.java
@@ -2,6 +2,19 @@ package tester;
 
 
 class Parent{
+    public static int getMinimum(int first, int second, int third){
+        int min = 0;
+        if (first <= second){
+            min = first;
+        }
+        else{
+            min = second;
+        }
+        if (third <= min){
+            min = third;
+        }
+        return min;
+    }
 
 }
 
