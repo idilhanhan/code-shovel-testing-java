cf0212ce3ee622109f08c327610e21294048d353
idilhanhan <idilhanhan@gmail.com>
2024-02-14 17:38:31 +0300

    update method

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static int getSum(int x, int y){
         int sum = x + y;
-        return x + y;
+        return sum;
     }
 
     public static void main(String[] args) {
