3ec08cc4d7097b90df2331c97964127789429449
idilhanhan <idilhanhan@gmail.com>
2024-02-14 17:50:21 +0300

    cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,12 @@
-
-
 public class Main {
     public static int getSum(int x, int y){
         int sum = x + y;
         return sum;
     }
 
-
     public static void main(String[] args) {
         System.out.println("Running Main.java");
         int maxValue = getSum(4, 5);
-        //int minValue = getMinimum(4, 5, 3);
-        //boolean result = Math.isEqual(3,3);
         System.out.println(maxValue);
-        //System.out.println(minValue);
-        //System.out.println(result);
     }
 }
