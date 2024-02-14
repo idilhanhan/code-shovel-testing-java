1151e341991266cc8628e979222ba1aa1ec047f5
idilhanhan <idilhanhan@gmail.com>
2024-02-14 16:16:47 +0300

    create new file and method

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,9 @@ public class Main {
         System.out.println("Running Main.java");
         int maxValue = getMaximum(4, 5);
         int minValue = getMinimum(4, 5);
+        boolean result = Tester.isEqual(3,3);
         System.out.println(maxValue);
         System.out.println(minValue);
+        System.out.println(result);
     }
 }
---
+++ src/Tester.java
@@ -0,0 +1,5 @@
+public class Tester {
+    public static boolean isEqual(int x, int y){
+        return x == y;
+    }
+}
