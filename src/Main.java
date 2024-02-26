17b8728dacad951e715ab2d32d832e79e7898577
idilhanhan <idilhanhan@gmail.com>
2024-02-26 12:14:05 +0300

    create SimpleMath class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 public class Main {
-    public static int getSum(int x, int y){
-        return x + y;
-    }
-
     public static void main(String[] args) {
         System.out.println("Running Main.java");
-        int maxValue = getSum(4, 5);
-        System.out.println(maxValue);
+        SimpleMath sm = new SimpleMath(4,5);
+        System.out.println(sm.getSum());
     }
 }
---
+++ src/SimpleMath.java
@@ -0,0 +1,12 @@
+public class SimpleMath {
+    int x, y;
+
+    public SimpleMath(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getSum(){
+        return x + y;
+    }
+}
