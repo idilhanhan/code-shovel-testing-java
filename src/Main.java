060e62ad3d1a61edda0d20934cd27e3cc9c0c29a
idilhanhan <idilhanhan@gmail.com>
2024-02-26 12:08:28 +0300

    update method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static int getSum(int x, int y){
-        int sum = x + y;
-        return sum;
+        return x + y;
     }
 
     public static void main(String[] args) {
